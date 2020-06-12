970ef5ad7208737c7de8a5318cca641ca4411db2
Admin <Admin@DESKTOP-GORFP1Q>
2020-06-12 08:27:06 +0530

    commit

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex1A.java
@@ -0,0 +1,20 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex1A {
+	public static void main(String args[])
+	{
+		int a=Integer.parseInt(args[0]);
+		if(a>0)
+		{
+			System.out.println("Positive");
+		}
+		else if(a==0)
+		{
+			System.out.println("Zero");
+		}
+		else
+		{
+			System.out.println("Negative");
+		}
+	}
+}
