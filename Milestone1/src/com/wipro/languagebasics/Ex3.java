fb3d81ee6b6f966407517b6f701d1765c9e116d3
Admin <Admin@DESKTOP-GORFP1Q>
2020-06-12 08:09:28 +0530

    commit

---
+++ Milestone1/src/com/wipro/languagebasics/Ex3.java
@@ -0,0 +1,10 @@
+package com.wipro.languagebasics;
+
+public class Ex3 {
+	public static void main(String args[])
+	{
+		int a=Integer.parseInt(args[3]);
+		int b=Integer.parseInt(args[4]);
+		System.out.println("The sum of "+a+" and "+b+" is "+(a+b));
+	}
+}
