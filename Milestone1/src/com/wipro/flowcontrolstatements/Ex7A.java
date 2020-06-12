9c8abe6b861daa957a6518e8c7bb527101627f64
Admin <Admin@DESKTOP-GORFP1Q>
2020-06-12 09:44:55 +0530

    commit

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex7A.java
@@ -0,0 +1,15 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex7A {
+public static void main(String[] args) {
+	char a='D';
+	if(a>='a'&&a<='z')
+	{
+		System.out.println("a-->"+Character.toUpperCase(a));
+	}
+	else
+	{	
+		System.out.println("a-->"+Character.toLowerCase(a));
+	}
+}
+}
