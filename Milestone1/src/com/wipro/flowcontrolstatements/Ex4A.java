e813f925af63f14ee69d64a46842ca9ad3df5fb7
Admin <Admin@DESKTOP-GORFP1Q>
2020-06-12 08:58:45 +0530

    commit

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex4A.java
@@ -0,0 +1,18 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex4A {
+	public static void main(String[] args) {
+		char a='e';
+		char b='a';
+		if(b>a)
+		{
+			System.out.println(""+a+" "+b);
+		}
+		else
+		{
+			System.out.println(""+b+" "+a);
+
+		}
+		
+	}
+}
