85d0268958e9428fc505ab789f2441a809c525fe
Admin <Admin@DESKTOP-GORFP1Q>
2020-06-13 07:24:45 +0530

    commit

--- Milestone1/src/com/wipro/flowcontrolstatements/Ex10A.java
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex10A.java
@@ -2,9 +2,10 @@ package com.wipro.flowcontrolstatements;
 
 public class Ex10A {
 	public static void main(String[] args) {
-		for(int i=1;i<=10;i++)
+		System.out.print("1");
+		for(int i=2;i<=10;i++)
 		{
-			System.out.print(""+i);
+			System.out.print("	"+i);
 		}
 	}
 }
