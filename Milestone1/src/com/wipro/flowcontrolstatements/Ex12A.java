ebb155a1b6675c224403160f649a970707b433fb
Admin <Admin@DESKTOP-GORFP1Q>
2020-06-13 07:48:20 +0530

    commit

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex12A.java
@@ -0,0 +1,23 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex12A {
+	public static void main(String args[]) {
+		int n=Integer.parseInt(args[0]);
+		int flag=0;
+		for(int i=0;i<-Math.sqrt(n);i++)
+		{
+			if(n % i == 0)
+            {
+                flag = 1;
+            }
+		}
+		if(flag==0)
+		{
+			System.out.println("Prime Number");
+		}
+		else
+		{
+			System.out.println("Not a Prime Number");
+		}
+	}
+}
