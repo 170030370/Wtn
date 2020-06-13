7e6125b0e294afb37d2fccdb3e6582c1417499a6
Admin <Admin@DESKTOP-GORFP1Q>
2020-06-13 07:29:34 +0530

    commit

---
+++ Milestone1/src/com/wipro/flowcontrolstatements/Ex11A.java
@@ -0,0 +1,13 @@
+package com.wipro.flowcontrolstatements;
+
+public class Ex11A {
+	public static void main(String[] args) {
+		for(int i=23;i<57;i++)
+		{
+			if((i&1)==0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
