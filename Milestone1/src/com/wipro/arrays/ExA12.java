f4b9d9e7638ccd1db5ffc0b8e1a3d7d95cf379c1
Admin <Admin@DESKTOP-GORFP1Q>
2020-06-15 09:17:38 +0530

    commit

---
+++ Milestone1/src/com/wipro/arrays/ExA12.java
@@ -0,0 +1,13 @@
+package com.wipro.arrays;
+
+public class ExA12 {
+public static void main(String[] args) {
+	int a[]= {1,2,3};
+	int b[]= {4,5,6};
+	int c[]= {a[1],b[1]};
+	for(int i=0;i<c.length;i++)
+	{
+		System.out.println(""+c[i]);
+	}
+ }
+}
