8930902783abfcd79b3d161e097577d03bcb9c02
Admin <Admin@DESKTOP-GORFP1Q>
2020-06-23 08:28:43 +0530

    commit

---
+++ Milestone1/src/com/wipro/encapsulationandabstraction/Book.java
@@ -0,0 +1,41 @@
+package com.wipro.encapsulationandabstraction;
+
+public class Book{
+	String name;
+	double price;
+	String author;
+	int qtyInstock;
+	Book(String name){
+	this.name=name;
+	}
+	public String getName() {
+	return name;
+	}
+	public double getPrice() {
+	return price;
+	}
+	public void setPrice(double price) {
+	this.price = price;
+	}
+	void getAuthor() {
+	System.out.println("Name is : "+Author.name);
+	System.out.println("email is : "+Author.email);
+	System.out.println("Gender is : "+Author.gender);
+	}
+	public int getQtyInstock() {
+	return qtyInstock;
+	}
+	public void setQtyInstock(int qtyInstock) {
+	this.qtyInstock = qtyInstock;
+	}
+	public static void main(String args[]) {
+	Book b=new Book("Tales");
+	Author a=new Author("Indu","Indu@gmail.com",'f');
+	b.setPrice(568.50);
+	b.setQtyInstock(89);
+	System.out.println("Name of book :"+b.getName());
+	System.out.println("Price of book :"+b.getPrice());
+	System.out.println("qtyIn stock of book :"+b.getQtyInstock());
+	b.getAuthor();
+	}	
+}
